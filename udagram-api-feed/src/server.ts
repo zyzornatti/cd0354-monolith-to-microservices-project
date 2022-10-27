3a066308dcc70ad66187cf5c5c46c10020926da8
zyzornatti <tobimustapha53@gmail.com>
2022-10-27 11:43:00 +0100

    done

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -2,11 +2,11 @@ import cors from 'cors';
 import express from 'express';
 import {sequelize} from './sequelize';
 
-import {IndexRouter} from './controllers/feed/v0/index.router';
+import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
 import {config} from './config/config';
-import {V0_FEED_MODELS} from './controllers/feed/v0/model.index';
+import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
