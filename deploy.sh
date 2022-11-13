6216af1693631872f91a849ad42df5ba9297a2eb
zyzornatti <tobimustapha53@gmail.com>
2022-11-13 15:38:19 +0100

    done

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
-kubectl delete secrets udagram-aws-env
-kubectl delete secrets udagramdb-env
-kubectl delete configmap udagram-env
-
-kubectl delete deploy backend-user
+kubectl delete deploy backend-user-api
 kubectl delete deploy backend-feed
 kubectl delete deploy reverseproxy
 kubectl delete deploy frontend
